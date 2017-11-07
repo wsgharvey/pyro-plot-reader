c8cbe21456fad8e7f51432ada91b540f5153eb6e
Will Harvey <williamharvey08@gmail.com>
2017-11-07 22:55:48 +0000

    working with importance sampling

--- graph.py
+++ graph.py
@@ -15,7 +15,7 @@ from helpers import fig2tensor,\
                     set_size_pixels
 
 
-def graph():
+def graph(return_image=False):
     """ generates a bar chart with a single bar """
     max_height = 10
     height, width = 200, 200
@@ -35,28 +35,33 @@ def graph():
     # get the graph as a matrix
     fig = set_size_pixels(fig, (width, height))
     image = Variable(fig2tensor(fig))
-    print(image)
 
     # observe the graph with some Gaussian noise
     noise_std = Variable(torch.ones(image.size()))
     observed_image = pyro.sample("observed_image",
                                  dist.normal,
-                                 image,
-                                 noise_std)
-    return bar_height, image
+                                 image.view(-1),
+                                 noise_std.view(-1))
+    # must return values in a structure that allows equality comparison
+    if return_image:
+        return bar_height, image
+    else:
+        return bar_height
 
 
 # make data:
-real_height, real_img = graph()
+real_height, real_img = graph(return_image=True)
+
+print("true height is", real_height)
 
 # condition on the data
 conditioned_graph = pyro.condition(
                         graph,
-                        data={"observed_image": real_img})
+                        data={"observed_image": real_img.view(-1)})
 
 # run inference
-posterior = pyro.infer.Importance(conditioned_graph, num_samples=100)
+posterior = pyro.infer.Importance(conditioned_graph, num_samples=5)
 marginal = pyro.infer.Marginal(posterior)
 
 # sample from empirical posterior
-print("graph is:\n", marginal())
+print("estimated height is:\n", marginal())
