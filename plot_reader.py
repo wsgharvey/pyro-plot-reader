4049b68c67cc9baeeda3549db0442eda9722044c
Will Harvey <williamharvey08@gmail.com>
2017-11-25 21:20:57 +0000

    not sure if it works but it runs

--- plot_reader.py
+++ plot_reader.py
@@ -24,17 +24,11 @@ class PlotReader(nn.Module):
         super(PlotReader, self).__init__()
         self.height = 200
         self.width = 200
-        self.bar_height_net = BarHeightNet()
-        self.abc_dist_size = 50
-        self.abc_projection = Variable(torch.randn(self.abc_dist_size,
-                                                   3*self.height*self.width)) / 1000
 
-    def model(self, observed_image,
-              return_image=False,
-              save_address=None):
+    def model(self,
+              observed_image=Variable(torch.zeros(200, 200))):
         """ generates a bar chart with a single bar
         """
-        print("running model")
         max_height = 10
         height, width = self.height, self.width
 
@@ -42,9 +36,6 @@ class PlotReader(nn.Module):
                                  dist.uniform,
                                  Variable(torch.Tensor([0])),
                                  Variable(torch.Tensor([max_height])))
-        # bar_height.register_hook(lambda grad: print("grad bar height is {}".format(grad.data.numpy())))
-
-        # plot the graph
         fig, ax = plt.subplots()
         ax.bar([1],
                bar_height.data,
@@ -53,63 +44,15 @@ class PlotReader(nn.Module):
 
         # get the graph as a matrix
         fig = set_size_pixels(fig, (width, height))
-        if save_address is not None:
-            fig.savefig(save_address)
         image = Variable(fig2tensor(fig))
         plt.close()
 
-        if observed_image is not None:
-            generated_image = image.view(-1)
-            noise_std = Variable(10*torch.ones(generated_image.size()))
-            observed_image = pyro.observe("observed_image",
-                                          dist.normal,
-                                          obs=observed_image.view(-1),
-                                          mu=generated_image,
-                                          sigma=noise_std)
-            # observed = pyro.observe("observed_image",
-            #                         dist.normal,
-            #                         bar_height,
-            #                         Variable(torch.Tensor([1])),
-            #                         Variable(torch.Tensor([1])))
-        # must return values in a structure that allows equality comparison
-        # when running inference
-        if return_image:
-            return bar_height, image
-        else:
-            return bar_height
+        generated_image = image.view(-1)
+        noise_std = Variable(10*torch.ones(generated_image.size()))
+        observed_image = pyro.observe("observed_image",
+                                      dist.normal,
+                                      obs=observed_image,
+                                      mu=generated_image,
+                                      sigma=noise_std)
 
-    def test_model(self):
-        z = pyro.sample("bar_height",
-                        dist.normal,
-                        mu=Variable(torch.Tensor([10])),
-                        sigma=Variable(torch.Tensor([1])))
-
-        # fuck up the gradients
-        z = Variable(torch.Tensor(z.data.numpy()))
-
-        # return pyro.observe("observation",
-        #                     dist.normal,
-        #                     obs=Variable(torch.Tensor([5])),
-        #                     mu=z,
-        #                     sigma=Variable(torch.Tensor([1])))
-
-    def guide(self):
-        print("running guide")
-
-        a0 = Variable(torch.Tensor([1]), requires_grad=True)
-        b0 = Variable(torch.Tensor([1]), requires_grad=True)
-        a0.register_hook(lambda grad: print("grad of a is {}".format(grad.data.numpy())))
-        b0.register_hook(lambda grad: print("grad of b is {}".format(grad.data.numpy())))
-        a = pyro.param("a", a0)
-        b = pyro.param("b", b0)
-
-        pyro.module("bar_height_net", self.bar_height_net)
-
-        # mean_height = self.bar_height_net(observed_image)
-        print("a is {}".format(a.data.numpy()))
-        print("b is {}".format(b.data.numpy()))
-        bar_height = pyro.sample("bar_height",
-                                 dist.normal,
-                                 mu=a,
-                                 sigma=b)
-        print("height sampled as", bar_height.data.numpy()[0])
+        return generated_image
--- test.py
+++ test.py
@@ -5,8 +5,7 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 
 import pyro
-import pyro.infer
-import pyro.optim
+import pyro.infer as infer
 import pyro.distributions as dist
 
 from plot_reader import PlotReader
@@ -18,44 +17,6 @@ from PIL import Image
 
 plot_reader = PlotReader()
 
-
-def data_generator(num_batches=-1, batch_size=8, use_cuda=False):
-    """ generator for stream of data batches
-        yields tuples of (
-                            batch_size x 3 x height x width Variable of image
-                            batch_size variable of true bar heights
-        )
-    """
-    plot_reader = PlotReader()
-    while True:
-        images = Variable(torch.zeros(batch_size,
-                                      200,
-                                      200,
-                                      3).type(torch.FloatTensor))
-        true_heights = Variable(torch.zeros(batch_size).type(torch.FloatTensor))
-
-        for i in range(batch_size):
-            true_heights[i], images[i] = plot_reader.model(None,
-                                                           return_image=True)
-
-        yield images, true_heights
-
-
-svi = pyro.infer.SVI(plot_reader.test_model,
-                     plot_reader.guide,
-                     pyro.optim.Adam({"lr": 0.01}),
-                     loss="ELBO",
-                     num_particles=5)
-
-# images, true_heights = next(data_generator())
-# image, true_height = images[0], true_heights[0]
-# print("height should be", true_height)
-#
-# from PIL import Image
-# img = Image.fromarray(image.data.numpy().astype(np.uint8), 'RGB')
-# img.show()
-
-
-while True:
-    print("loss is", svi.step())
-    print("\n")
+csis = infer.CSIS(model=plot_reader.model,
+                  optim=torch.optim.Adam)
+csis.compile(n_steps=10)
