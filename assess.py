0cd3ebed4175bd32a87131c72fb801cb3cc5b127
Will Harvey <williamharvey08@gmail.com>
2017-12-22 13:59:45 +0000

    assessing error for multi-bar plots

--- assess.py
+++ assess.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from file_paths import DATASET_PATH
 
 sse = 0
@@ -6,8 +8,10 @@ with open("{}/test/targets.csv".format(DATASET_PATH)) as targets,\
         open("{}/test_predictions.csv".format(DATASET_PATH)) as estimates:
     for target, estimate in zip(targets, estimates):
         n += 1
-        error = float(target) - float(estimate)
-        sse += error**2
+        target_vec = np.array(list(map(float, target.split(","))))
+        estimate_vec = np.array(list(map(float, estimate.split(","))))
+        diff = target_vec - estimate_vec
+        sse += sum(diff**2)/len(target_vec)
 
 rmse = (sse/n)**0.5
 
