74e4c1c998ce55d5610c8eba5e6fae6f39f4b78d
Will Harvey <williamharvey08@gmail.com>
2018-04-19 11:16:42 +0100

    fixed to properly evaluate log pdf

--- plot_and_score.py
+++ plot_and_score.py
@@ -29,41 +29,14 @@ args = parser.parse_args()
 print("CUDA:", bool(args.cuda))
 
 artifact = PersistentArtifact.load(args.artifact)
-inference_log = artifact.infer(args.dataset,
-                               attention_plots=True,
-                               cuda=bool(args.cuda),
-                               max_plots=args.N)
+log_pdf = artifact.infer(args.dataset,
+                         attention_plots=True,
+                         cuda=bool(args.cuda),
+                         max_plots=args.N)
 
 targets_file = open("{}/{}/test/targets.csv".format(DATASET_FOLDER, args.dataset), 'r')
 
-num_incorrect_bar_counts = 0
-num_correct_bar_counts = 0
-log_pdf = 0
-
-num_bars = 0
-
-for trace in inference_log:
-    true_values = targets_file.readline().strip('\n').split(',')
-
-    true_num_bars = len(true_values)
-    if "bar_height_{}".format(true_num_bars-1) in trace\
-            and "bar_height_{}".format(true_num_bars) not in trace:
-
-        for i, true_value in enumerate(map(float, true_values)):
-            proposal_params = trace["bar_height_{}".format(i)]
-            mode, certainty = proposal_params
-            proposal_dist = UniformProposal(Variable(torch.Tensor([0.])),
-                                            Variable(torch.Tensor([10.])),
-                                            mode*10,
-                                            certainty)
-            log_pdf += proposal_dist.log_prob(Variable(torch.Tensor([true_value])))
-            num_bars += 1
-        num_correct_bar_counts += 1
-
-    else:
-        num_incorrect_bar_counts += 1
-
-mean_log_pdf = log_pdf / num_bars
+print(log_pdf)
 
 if args.L is not None:
     scores = pickle.load(open(args.L, 'rb'))
