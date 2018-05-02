f97c096cad6ca6b75c4028814a95142ab97dee0e
Will Harvey <williamharvey08@gmail.com>
2018-05-02 14:29:31 +0100

    changed to calculate and save 95% confidence intervals for bar heights during inference

--- artifact.py
+++ artifact.py
@@ -16,6 +16,7 @@ from helpers import image2variable
 from PIL import Image
 import numpy as np
 from scipy.special import logsumexp
+from scipy.stats import beta
 
 import pickle
 import os
@@ -126,6 +127,7 @@ class PersistentArtifact(object):
         guide = Guide(**guide_kwargs)
         guide.load_state_dict(torch.load(self.paths["weights"]))
 
+        text = ""
         img_no = start_no
         dataset_log_pdf = 0
         while img_no < start_no + max_plots and os.path.isfile("{}/graph_{}.png".format(test_folder, img_no)):
@@ -145,22 +147,23 @@ class PersistentArtifact(object):
             dataset_log_pdf += log_pdf
 
             datum_history = guide.get_history()[-T:]
-            # print("NUM BARS:", guide.get_history()[-T:][-1])
-            example = datum_history[0]
-            bar_height_sample_names = [name for name in example if name[:10] == 'bar_height']
             text += "inference on data point {}:\n".format(img_no)
-
-            for sample_name in bar_height_sample_names:
-                bar_height_predictions = []
-                for trace in datum_history:
-                    params = trace[sample_name]
-                    mode, cert = params
-                    mode, cert = mode.item(), cert.item()
-                    norm_m = mode
-                    norm_c = cert + 2
-                    dist = beta(norm_m * (norm_c - 2),
-                                (1 - norm_m) * (norm_c - 2))
-                    bar_height_predictions.append(dist)
+            bar_height_predictions = []
+            bar_no = 0
+            while True:
+                try:
+                    for trace in datum_history:
+                        params = trace["bar_height_{}".format(bar_no)]
+                        mode, cert = params
+                        mode, cert = mode.item(), cert.item()
+                        norm_m = mode
+                        norm_c = cert + 2
+                        dist = beta(norm_m * (norm_c - 2),
+                                    (1 - norm_m) * (norm_c - 2))
+                        bar_height_predictions.append(dist)
+                    bar_no += 1
+                except KeyError:
+                    break
                 confidence_intervals = []
                 # find where sum of cdfs in predictions add up to 0.05*T and 0.095*T
                 for target in [0.025*T, 0.975*T]:
@@ -173,10 +176,11 @@ class PersistentArtifact(object):
                         else:
                             lower = guess
                     confidence_intervals.append(guess)
-                text += sample_name + ": " + str(confidence_intervals) + "\n"
-            img_no += 1
+                text += "bar_height_{}".format(bar_no) + ": " + str(confidence_intervals) + "\n"
 
         inference_log = guide.get_history()
+        with open("{}/confidence_intervals".format(self.directory), 'w') as f:
+            f.write(text)
 
         pickle.dump(inference_log, open(self.paths["infer_log"], 'wb'))
         return dataset_log_pdf
