beb228d76efdfbe5d9a8bb8906ad076f1383fa7c
William Harvey <williamharvey08@gmail.com>
2018-05-11 02:37:10 -0700

    now must specify number of traces to use to calculate log prob
    Conflicts:
    	artifact.py
    	plot_and_score.py

--- plot_and_score.py
+++ plot_and_score.py
@@ -23,7 +23,7 @@ parser.add_argument("dataset", help="Name of dataset to use", type=str)
 parser.add_argument("cuda", help="Whether to use GPU", type=int)
 parser.add_argument("-N", help="Maximum number of plots to run inference on", type=int, default=np.inf)
 parser.add_argument("-L", help="Path to file to save the loss to", type=str)
-
+parser.add_argument("-T", help="Number of traces to take when running importance sampling on each sample", type=int)
 args = parser.parse_args()
 
 print("CUDA:", bool(args.cuda))
@@ -31,14 +31,18 @@ print("CUDA:", bool(args.cuda))
 artifact = PersistentArtifact.load(args.artifact)
 
 log_pdf = 0
-for start_no in range(0, 100, 100):
-
-    log_pdf += artifact.infer(args.dataset,
-                              attention_plots=True,
-                              start_no=start_no,
-                              cuda=bool(args.cuda), 
-                              max_plots=100)
-
+failed = 0
+for start_no in range(0, args.N, 1):
+    try:
+        log_pdf += artifact.infer(args.dataset,
+                                  attention_plots=True,
+                                  start_no=start_no,
+                                  cuda=bool(args.cuda),
+                                  max_plots=1,
+                                  num_traces=args.T)
+    except:
+        pass
+        failed = failed+1
 
 targets_file = open("{}/{}/test/targets.csv".format(DATASET_FOLDER, args.dataset), 'r')
 
